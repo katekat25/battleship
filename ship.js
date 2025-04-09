24f7180acdd1bf9e05cfc09899a3f18005fd8f85
Kate Schumacher <kate.schumacher@gmail.com>
2025-04-09 14:52:25 -0500

    Write ship class

---
+++ ship.js
@@ -0,0 +1,20 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.timesHit = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        this.timesHit++;
+    }
+
+    isSunk() {
+        if (this.timesHit === this.length) {
+            this.sunk = true;
+            return true;
+        } else return false;
+    }
+}
+
+export { Ship }
---
+++ ship.test.js
@@ -0,0 +1,20 @@
+import { Ship } from "./ship";
+
+test('Ship class exists', () => {
+    expect(new Ship()).toBeInstanceOf(Ship);
+});
+
+test('Hit() increases number of hits on ship', () => {
+    const ship = new Ship(1);
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+})
+
+test('isSunk() reports sinkage accurately', () => {
+    const ship = new Ship(4);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
+})
