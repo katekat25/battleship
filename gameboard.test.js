bf0f1520faeb33f8303c74f1738fcaa18aafb14c
Kate Schumacher <kate.schumacher@gmail.com>
2025-04-10 15:02:43 -0500

    Fix coordinate typo in tests

--- gameboard.test.js
+++ gameboard.test.js
@@ -8,9 +8,9 @@ test('Gameboard class exists', () => {
 test('Gameboard places ships at correct location', () => {
     let game = new Gameboard();
     game.placeShip(4, 0, 0, 3, 0);
-    expect(game.grid[0, 0].ship).not.toBeNull();
-    expect(game.grid[1, 0].ship).not.toBeNull();
-    expect(game.grid[2, 0].ship).not.toBeNull();
-    expect(game.grid[3, 0].ship).not.toBeNull();
-    expect(game.grid[4, 0].ship).toBeUndefined();
+    expect(game.grid[0][0].ship).not.toBeNull();
+    expect(game.grid[1][0].ship).not.toBeNull();
+    expect(game.grid[2][0].ship).not.toBeNull();
+    expect(game.grid[3][0].ship).not.toBeNull();
+    expect(game.grid[4][0].ship).toBeNull();
 })
